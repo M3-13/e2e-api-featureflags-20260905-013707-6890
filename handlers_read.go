bee494084f7e3aa751d6f008341c121c80436f4b
Leo <83591526+M3-13@users.noreply.github.com>
2026-09-05 01:52:35 +0200

    feat: Flag-Liste und Einzelabruf implementieren (#12)
    Closes #3
    
    Co-authored-by: office-crew <development@office-crew.dev>

--- handlers_read.go
+++ handlers_read.go
@@ -1,11 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func handleListFlags(w http.ResponseWriter, r *http.Request, s *Store) {
-	writeError(w, http.StatusNotImplemented, "not implemented")
+	flags := s.List()
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(flags)
 }
 
 func handleGetFlag(w http.ResponseWriter, r *http.Request, s *Store) {
-	writeError(w, http.StatusNotImplemented, "not implemented")
+	key := r.PathValue("key")
+	flag, ok := s.Get(key)
+	if !ok {
+		writeError(w, http.StatusNotFound, "flag not found")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(flag)
 }
